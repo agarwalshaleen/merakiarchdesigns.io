d50e0f0cdc22b4cc402bfd699bf762d97f9cf89d
Shaleen Agarwal <shaleen.agarwal@airtel.com>
2022-09-16 20:42:32 +0530

    First Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectComponent } from './project/project.component';
+
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'projects', component: ProjectComponent
+  },
+  {
+    path: 'aboutus', component: AboutComponent
+  },
+  {
+    path: 'contact', component: ContactComponent
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
